ee965045fc6bd48450de826ec8e2042b899f9984
chesky <sye0534184736@gmail.com>
2026-06-03 15:23:47 +0300

    update io file with logs and exeptions

--- exercises/project/utils/io.py
+++ exercises/project/utils/io.py
@@ -4,12 +4,21 @@ import logger_config
 DATA = "soldiers.json"
 
 def load_json():
-    with open(DATA , "r",encoding="utf-8") as soldiers:
-        return json.load(soldiers)
-
+    try:
+        with open(DATA, "r", encoding="utf-8") as soldiers_file:
+            data = json.load(soldiers_file)
+            logger_config.logger.info("Successfully loaded data from JSON")
+            return data
+    except FileNotFoundError:
+        logger_config.logger.error("Could not find soldiers.json file!")
+        return []
         
 def save_json(soldiers):
-    with open(DATA ,"w" ,encoding="utf-8") as soldiers_file:
-        json.dump(soldiers ,soldiers_file ,indent=4)
+    try:
+        with open(DATA ,"w" ,encoding="utf-8") as soldiers_file:
+            json.dump(soldiers ,soldiers_file ,indent=4)
+            logger_config.logger.info("Save soldiers list to JSON")
+    except FileNotFoundError:
+        logger_config.logger.error("Could not find soldiers.json file!")
 
 
